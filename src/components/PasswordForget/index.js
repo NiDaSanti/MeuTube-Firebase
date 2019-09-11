a898f989cda8c2bbc908c6990da91e925230905d
mamooredesigns <mamooredesigns@gmail.com>
2019-09-11 16:44:02 -0400

    password forget and change complete and operational

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -2,21 +2,25 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
+
 const PasswordForgetPage = () => (
   <div>
     <h1>PasswordForget</h1>
     <PasswordForgetForm />
   </div>
 );
+
 const INITIAL_STATE = {
   email: '',
   error: null,
 };
+
 class PasswordForgetFormBase extends Component {
   constructor(props) {
     super(props);
     this.state = { ...INITIAL_STATE };
   }
+
   onSubmit = event => {
     const { email } = this.state;
     this.props.firebase
@@ -29,9 +33,11 @@ class PasswordForgetFormBase extends Component {
       });
     event.preventDefault();
   };
+
   onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
+
   render() {
     const { email, error } = this.state;
     const isInvalid = email === '';
@@ -44,19 +50,26 @@ class PasswordForgetFormBase extends Component {
           type="text"
           placeholder="Email Address"
         />
+
         <button disabled={isInvalid} type="submit">
           Reset My Password
         </button>
+
         {error && <p>{error.message}</p>}
+
       </form>
     );
   }
 }
+
 const PasswordForgetLink = () => (
   <p>
     <Link to={ROUTES.PASSWORD_FORGET}>Forgot Password?</Link>
   </p>
 );
+
 export default PasswordForgetPage;
+
 const PasswordForgetForm = withFirebase(PasswordForgetFormBase);
+
 export { PasswordForgetForm, PasswordForgetLink };

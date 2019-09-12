554e7e92333dc89340a5d92cd6f0320eb20717f0
mamooredesigns <mamooredesigns@gmail.com>
2019-09-12 17:51:30 -0400

    part 2 done up to how to merge auth user with db user

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -4,6 +4,7 @@ import { compose } from 'recompose';
 
 import { FirebaseContext, withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
+import * as ROLES from '../../constants/roles';
 
 const SignUpPage = () => (
   <div>
@@ -19,6 +20,7 @@ const INITIAL_STATE = {
     email: '',
     passwordOne: '',
     passwordTwo: '',
+    isAdmin: false,
     error: null,
 };
 
@@ -30,7 +32,12 @@ class SignUpFormBase extends Component {
   }
 
   onSubmit = event => {
-      const { username, email, passwordOne } = this.state;
+      const { username, email, passwordOne, isAdmin } = this.state;
+      const roles = {};
+
+      if (isAdmin) {
+        roles[ROLES.ADMIN] = ROLES.ADMIN;
+      }
 
       this.props.firebase
         .doCreateUserWithEmailAndPassword(email, passwordOne)
@@ -41,6 +48,7 @@ class SignUpFormBase extends Component {
               .set({
                 username,
                 email,
+                roles,
               });
             })
         .then(() => {
@@ -58,12 +66,17 @@ class SignUpFormBase extends Component {
       this.setState({ [event.target.name]: event.target.value });
   };
   
+  onChangeCheckbox = event => {
+    this.setState({ [event.target.name]: event.target.checked });
+  };
+
   render() {
     const {
         username,
         email,
         passwordOne,
         passwordTwo,
+        isAdmin,
         error,
     } = this.state;
 
@@ -103,6 +116,15 @@ class SignUpFormBase extends Component {
           type="password"
           placeholder="Confirm Password"
         />
+        <label>
+          Admin:
+          <input
+            name="isAdmin"
+            type="checkbox"
+            checked={isAdmin}
+            onChange={this.onChangeCheckbox}
+          />
+        </label>
         <button disabled={isInvalid} type="submit">Sign Up</button>
 
         {error && <p>{error.message}</p>}

58ebe4c522da105e4df7a4131098eec062bbb4d2
mamooredesigns <mamooredesigns@gmail.com>
2019-09-16 12:27:30 -0400

    admin page styled

---
+++ src/containers/Admin/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdminPage from '../../components/Admin';
+
+const Admin = () => {
+    return(
+        <div className="adminPageView">
+            <AdminPage />
+        </div>
+    )
+}
+
+export default Admin
